a166081330e4aad03c868906dace2bed61ddfd90
Colinho22 <48288595+Colinho22@users.noreply.github.com>
2021-05-23 13:25:10 +0200

    App Server Docker
    Push Code For App Deployment on Docker Desktop

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+//App
+const app = express();
+app.get('/', (req, res,) => {res.send('Hello There :) - Ah General Kenobi -.-')});
+
+app.listen(PORT,HOST);
+console.log('Running on http://${HOST}:${PORT}');
